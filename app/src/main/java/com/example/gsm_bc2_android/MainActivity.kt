6cce7d23beab215b560040b00317a9290fd64ffb
younghyun9792 <s21020@gsm.hs.kr>
2022-12-20 11:44:51 +0900

    first page

--- app/src/main/java/com/example/gsm_bc2_android/MainActivity.kt
+++ app/src/main/java/com/example/gsm_bc2_android/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.gsm_bc2_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.main)
+//    }
+//
+//
+//    }
+//}
