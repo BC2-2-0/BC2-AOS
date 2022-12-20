b0e5c34c80e3b5ba0ba4fbff63df5a0751e82423
younghyun9792 <s21020@gsm.hs.kr>
2022-12-20 23:34:41 +0900

    action bar remove

---
+++ app/src/main/java/com/example/gsm_bc2_android/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.example.gsm_bc2_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.main)
+//    }
+//
+//
+//    }
+//}
