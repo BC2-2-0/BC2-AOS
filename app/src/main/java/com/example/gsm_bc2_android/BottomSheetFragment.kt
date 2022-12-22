320ca2a87dd3d4e0163dcb0850fb14be95eb218c
younghyun9792 <s21020@gsm.hs.kr>
2022-12-22 19:07:33 +0900

    nfc connect success

--- app/src/main/java/com/example/gsm_bc2_android/BottomSheetFragment.kt
+++ app/src/main/java/com/example/gsm_bc2_android/BottomSheetFragment.kt
@@ -3,56 +3,63 @@ package com.example.gsm_bc2_android
 import android.app.Activity
 import android.app.Dialog
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.example.gsm_bc2_android.NfcActivity
 import com.example.gsm_bc2_android.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class BottomSheetFragment: BottomSheetDialogFragment() {
-
-//    private lateinit var nfcPendingIntent: PendingIntent
-//    private lateinit var nfcAdapter: NfcAdapter
-
-
+class BottomSheetFragment(): BottomSheetDialogFragment() {
+    private var TAG: String = "HomeActivity"
+    private lateinit var nfcPendingIntent: PendingIntent
+    private lateinit var nfcAdapter: NfcAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
     }
-
 
     companion object {
         const val TAG = "BottomSheetFragment"
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-//        nfcPendingIntent = PendingIntent.getActivity(
-//            this, 0,
-//            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
-//        )
         val dialog: Dialog = super.onCreateDialog(savedInstanceState)
         dialog.setOnShowListener {
             val bottomSheetDialog = it as BottomSheetDialog
             // 다이얼로그 크기 설정 (인자값 : DialogInterface)
             setupRatio(bottomSheetDialog)
         }
+
         return dialog
     }
 
 
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this);
+    }
 
     private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
         //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
--- app/src/main/java/com/example/gsm_bc2_android/HomeActivity.kt
+++ app/src/main/java/com/example/gsm_bc2_android/HomeActivity.kt
@@ -1,20 +1,30 @@
 package com.example.gsm_bc2_android
 
-import BottomSheetFragment
+//import BottomSheetFragment
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.nfc.*
+import android.nfc.tech.Ndef
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HomeActivity : AppCompatActivity() {
+
+    private var TAG: String = "HomeActivity"
+    private lateinit var nfcPendingIntent: PendingIntent
+    private lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
 
-        //왼쪽 위에 구글 사용자 이름+안녕하세요 띄어주기
+        //왼쪽 위에 구글 사용자 이름+안녕하세요!! 띄어주기
         val user_name = intent.getStringExtra("username")
         Log.d("name_HomeActivity",user_name.toString())
         findViewById<TextView>(R.id.xml_username).text = user_name+"님\n안녕하세요!!"
@@ -23,5 +33,55 @@ class HomeActivity : AppCompatActivity() {
             val bottomSheet = BottomSheetFragment()
             bottomSheet.show(supportFragmentManager, BottomSheetFragment.TAG)
         }
+
+        findViewById<Button>(R.id.nfcpage).setOnClickListener(){
+            val intent = Intent(this, NfcActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+
+        val manager = getSystemService(Context.NFC_SERVICE) as NfcManager
+        nfcAdapter = manager.defaultAdapter
+        nfcPendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
     }
+
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val detectedTag : Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        val writeValue : String = "50000";
+        val message: NdefMessage = createTagMessage(writeValue);
+
+        if (detectedTag != null) {
+            writeTag(message, detectedTag)
+        };
+    }
+
+    private fun createTagMessage(msg: String): NdefMessage {
+        return NdefMessage(NdefRecord.createUri(msg))
+    }
+
+    fun writeTag(message: NdefMessage, tag: Tag) {
+        val size = message.toByteArray().size
+        try {
+            val ndef = Ndef.get(tag)
+            if (ndef != null) {
+                ndef.connect()
+                if (!ndef.isWritable) {
+                    Toast.makeText(applicationContext, "can not write NFC tag", Toast.LENGTH_SHORT).show()
+                }
+                if (ndef.maxSize < size) {
+                    Toast.makeText(applicationContext, "NFC tag size too large", Toast.LENGTH_SHORT).show()
+                }
+                ndef.writeNdefMessage(message)
+                Toast.makeText(applicationContext, "NFC tag is writted", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            //Log.i(TAG,e.message);
+        }
+    }
+
 }
