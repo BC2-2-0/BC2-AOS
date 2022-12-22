ecb6c84da047542db76681a422160c507c3bd847
younghyun9792 <s21020@gsm.hs.kr>
2022-12-22 10:25:00 +0900

    nfc tag page lottie add

---
+++ app/src/main/java/com/example/gsm_bc2_android/BottomSheetFragment.kt
@@ -0,0 +1,78 @@
+package com.example.gsm_bc2_android
+
+import android.app.Activity
+import android.app.Dialog
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.gsm_bc2_android.NfcActivity
+import com.example.gsm_bc2_android.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment: BottomSheetDialogFragment() {
+
+//    private lateinit var nfcPendingIntent: PendingIntent
+//    private lateinit var nfcAdapter: NfcAdapter
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+    }
+
+
+    companion object {
+        const val TAG = "BottomSheetFragment"
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+//        nfcPendingIntent = PendingIntent.getActivity(
+//            this, 0,
+//            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+//        )
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {
+            val bottomSheetDialog = it as BottomSheetDialog
+            // 다이얼로그 크기 설정 (인자값 : DialogInterface)
+            setupRatio(bottomSheetDialog)
+        }
+        return dialog
+    }
+
+
+
+    private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
+        //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
+        //id = android.support.design.R.id.design_bottom_sheet for support librares
+        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val behavior = BottomSheetBehavior.from<View>(bottomSheet)
+        val layoutParams = bottomSheet.layoutParams
+        layoutParams.height = getBottomSheetDialogDefaultHeight()
+        bottomSheet.layoutParams = layoutParams
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun getBottomSheetDialogDefaultHeight(): Int {
+        return getWindowHeight() * 84 / 100
+    }
+
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+}
