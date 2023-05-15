66a2176a27bf7c31c08f03c18f517d742fc08113
younghyun9792 <s21020@gsm.hs.kr>
2023-05-15 20:17:30 +0900

    bottomsheet end

--- app/src/main/java/com/example/gsm_bc2_android/BottomSheetFragment.kt
+++ app/src/main/java/com/example/gsm_bc2_android/BottomSheetFragment.kt
@@ -3,26 +3,49 @@ package com.example.gsm_bc2_android
 import android.app.Activity
 import android.app.Dialog
 import android.app.PendingIntent
+import android.content.Intent
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class BottomSheetFragment(): BottomSheetDialogFragment() {
-    private var TAG: String = "HomeActivity"
+class BottomSheetFragment() : BottomSheetDialogFragment() {
     private lateinit var nfcPendingIntent: PendingIntent
     private lateinit var nfcAdapter: NfcAdapter
+    lateinit var Charge_Button: ConstraintLayout
+    lateinit var Mining_Button: ConstraintLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view: View = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+        val currentActivity = activity
+
+        Charge_Button = view.findViewById(R.id.select_charge)
+        Mining_Button = view.findViewById(R.id.select_mining)
+
+        Charge_Button.setOnClickListener {
+            startActivity(Intent(currentActivity, ChargeActivity::class.java))
+            currentActivity?.finish()
+            Log.d(TAG, "onCreateView: Charge")
+        }
+
+        Mining_Button.setOnClickListener {
+            startActivity(Intent(currentActivity, GameActivity::class.java))
+            currentActivity?.finish()
+            Log.d(TAG, "onCreateView: Mining")
+        }
+
+        return view
     }
 
     companion object {
@@ -35,26 +58,17 @@ class BottomSheetFragment(): BottomSheetDialogFragment() {
             val bottomSheetDialog = it as BottomSheetDialog
             // 다이얼로그 크기 설정 (인자값 : DialogInterface)
             setupRatio(bottomSheetDialog)
+
         }
 
         return dialog
     }
 
-
-//    override fun onResume() {
-//        super.onResume()
-//        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        nfcAdapter.disableForegroundDispatch(this);
-//    }
-
     private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
         //id = com.google.android.material.R.id.design_bottom_sheet for Material Components
         //id = android.support.design.R.id.design_bottom_sheet for support librares
-        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
+        val bottomSheet =
+            bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as View
         val behavior = BottomSheetBehavior.from<View>(bottomSheet)
         val layoutParams = bottomSheet.layoutParams
         layoutParams.height = getBottomSheetDialogDefaultHeight()
@@ -63,7 +77,7 @@ class BottomSheetFragment(): BottomSheetDialogFragment() {
     }
 
     private fun getBottomSheetDialogDefaultHeight(): Int {
-        return getWindowHeight() * 75 / 100
+        return getWindowHeight() * 35 / 100
     }
 
     private fun getWindowHeight(): Int {
